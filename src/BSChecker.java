6911c8b525c9281538a49c00158fb6db7b2f4740
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-15 11:37:17 -0400

    BSchecker is now a test class

--- src/BSChecker.java
+++ src/BSChecker.java
@@ -1,4 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import opennlp.tools.tokenize.*;
+import opennlp.tools.util.*;
 public class BSChecker {
-
-}
+	public static void main(String[] args) throws InvalidFormatException, IOException {
+			InputStream is = new FileInputStream("en-token.bin");
+		 
+			TokenizerModel model = new TokenizerModel(is);
+		 
+			Tokenizer tokenizer = new TokenizerME(model);
+		 
+			String tokens[] = tokenizer.tokenize("Hi. How are you? This is Mike.");
+		 
+			for (String a : tokens)
+				System.out.println(a);
+		 
+			is.close();
+		}
+	}
