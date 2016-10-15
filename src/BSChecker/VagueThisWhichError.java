6fb97dd2ef5ff5c7121d8711f6ea1fe6f7a0ba2b
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-15 14:08:48 -0400

    Remove bad comments

--- src/BSChecker/VagueThisWhichError.java
+++ src/BSChecker/VagueThisWhichError.java
@@ -26,50 +26,9 @@ public class VagueThisWhichError extends Error {
 		ArrayList<int[]> errs = new VagueThisWhichError().findErrors("Hi, my name is slim shady. Which is fun!");
 		for(int[] err: errs){
 			System.out.println(err[0] + " " + err[1] );
-			System.out.println("Hi, my name is slim shady. Which is fun!".substring(21));
+			System.out.println("Hi, my name is slim shady. Which is fun!".substring(27,32));
 		}
 	}
-	/*public ArrayList<int[]> findErrors(String text){
-		ArrayList<int[]> found = new ArrayList<int[]>();
-		
-		POSModel model = new POSModelLoader().load(new File("lib/en-pos-maxent.bin"));
-		POSTaggerME tagger = new POSTaggerME(model);
-		ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(text));
-		String line;
-		int lineNum = 0;
-		try {
-			while ((line = lineStream.read()) != null) {
-
-				String tokens[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
-				String[] tags = tagger.tag(tokens);
-				int last = 0;
-				while((last = Error.findWord(tokens, "this", last))!=-1){
-					if(last == tokens.length-1 || tags[last+1].charAt(0)!='N'){
-						//This error has occured!
-						int[] err = {lineNum,last,last};
-						found.add(err);
-					}
-					
-					last++;
-				}
-				last = 0;
-				while((last=Error.findWord(tokens, "which", last))!=-1){
-					System.out.println("Which found!");
-					if(last == 0 || tags[last-1].charAt(0)!='N'){
-						//Which error has occured!
-						int[] err = {lineNum,last,last};
-						found.add(err);
-					}
-					last++;
-				}
-				lineNum++;
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return found;
-	}*/
 	
 	public ArrayList<int[]> findErrors(String text){
 		
@@ -88,39 +47,18 @@ public class VagueThisWhichError extends Error {
 				for(int i = 0; i < tokens.length; i++){
 					if(tokens[i].equalsIgnoreCase("this")){
 						if(i == tokens.length-1 || tags[i+1].charAt(0)!='N'){
-							int[] err = {totLen+lineLen,totLen+lineLen};
+							int[] err = {totLen+lineLen,totLen+lineLen+tokens[i].length()};
 							found.add(err);
 						}
 					}
 					if(tokens[i].equalsIgnoreCase("which")){
 						if(i == 0 || tags[i-1].charAt(0)!='N' || endOfSentence(tokens[i-1])){
-							int[] err = {totLen+lineLen,totLen+lineLen};
+							int[] err = {totLen+lineLen,totLen+lineLen+tokens[i].length()};
 							found.add(err);
 						}
 					}
-					lineLen+=tokens[i].length();
+					lineLen+=tokens[i].length()+1;
 				}
-				/*int last = 0;
-				System.out.println(line);
-				while((last = Error.findWord(tokens, "this", last))!=-1){
-					if(last == tokens.length-1 || tags[last+1].charAt(0)!='N'){
-						//This error has occured!
-						int[] err = {lineNum,last,last};
-						found.add(err);
-					}
-					
-					last++;
-				}
-				last = 0;
-				while((last=Error.findWord(tokens, "which", last))!=-1){
-					System.out.println("Which found, " + tokens[last-1]);
-					if(last == 0 || tags[last-1].charAt(0)!='N' || endOfSentence(tokens[last-1])){
-						//Which error has occured!
-						int[] err = {lineNum,last,last};
-						found.add(err);
-					}
-					last++;
-				}*/
 				totLen+=line.length();
 			}
 		} catch (IOException e) {
