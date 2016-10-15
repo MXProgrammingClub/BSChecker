5522f2970103ebd107d1bfbc749892e2836c93b4
juliamcclellan <jmcclellan@mxschool.edu>
2016-10-15 15:37:36 -0400

    Fixing classpath

---
+++ src/BSChecker/QuotationForm.java
@@ -0,0 +1,15 @@
+package BSChecker;
+
+import java.util.ArrayList;
+
+public class QuotationForm extends Error
+{
+	
+	@Override
+	public ArrayList<int[]> findErrors(String text)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
