345f0d624310daf8f729be90213070d2d35e448e
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-15 13:17:43 -0400

    VagueThisError now works!

--- src/BSChecker/Error.java
+++ src/BSChecker/Error.java
@@ -9,14 +9,15 @@ public abstract class Error {
 	/**
 	 * 
 	 * @param text The block of text to find errors in
-	 * @return an ArrayList of int[2] pointers to the start and end of the error
-	 * 			int[0],int[1] are start and end of problem in string
+	 * @return an ArrayList of int[3] pointers to the line, start and end token of the error
+	 * 			int[0] is the line index,
+	 * 			int[1],int[2] are start and end tokens in error
 	 */
 	public abstract ArrayList<int[]> findErrors(String text);
 	
 	public static int findWord(String[] tokens, String text, int start){
 		for(int i = start; i < tokens.length; i++){
-			if(tokens[i].equals(text)) return i;
+			if(tokens[i].equalsIgnoreCase(text)) return i;
 		}
 		return -1;
 	}
--- src/BSChecker/VagueThisWhichError.java
+++ src/BSChecker/VagueThisWhichError.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import opennlp.tools.cmdline.PerformanceMonitor;
 import opennlp.tools.cmdline.postag.POSModelLoader;
@@ -17,33 +18,39 @@ import opennlp.tools.util.PlainTextByLineStream;
 public class VagueThisWhichError extends Error {
 
 	public static void main(String[] args){
-		System.out.println(new VagueThisWhichError().findErrors("Hi, my name is slim shady. I own a car!"));
+		ArrayList<int[]> errs = new VagueThisWhichError().findErrors("Hi, my name is slim shady. This is fun!");
+		for(int[] err: errs){
+			System.out.println(err[0] + " " + err[1] + " " + err[2]);
+		}
 	}
 	public ArrayList<int[]> findErrors(String text){
 		ArrayList<int[]> found = new ArrayList<int[]>();
+		
 		POSModel model = new POSModelLoader().load(new File("lib/en-pos-maxent.bin"));
 		POSTaggerME tagger = new POSTaggerME(model);
-
-		String input = "Hi. How are you? This is Mike.";
 		ObjectStream<String> lineStream = new PlainTextByLineStream(new StringReader(text));
-
 		String line;
+		int lineNum = 0;
 		try {
 			while ((line = lineStream.read()) != null) {
 
 				String tokens[] = WhitespaceTokenizer.INSTANCE.tokenize(line);
 				String[] tags = tagger.tag(tokens);
-				line.indexOf("this")
-				
-				POSSample sample = new POSSample(tokens, tags);
-				sample.
-				System.out.println(sample);
+				int last = 0;
+				while((last = Error.findWord(tokens, "this", last))!=-1){
+					if(last == tokens.length-1 || tags[last+1].charAt(0)!='N'){
+						//Error has occured!
+						int[] err = {lineNum,last,last};
+						found.add(err);
+					}
+					last++;
+				}
+				lineNum++;
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return found;
 	}
 
