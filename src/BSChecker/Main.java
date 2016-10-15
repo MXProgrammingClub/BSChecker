7501333945c5441eed9455f58c344bafee482945
giacalone99 <giacalone99@gmail.com>
2016-10-15 10:56:25 -0400

    Updating Main to Use JavaFX

--- src/BSChecker/Main.java
+++ src/BSChecker/Main.java
@@ -1,5 +1,28 @@
+/**
+ * The main class for the BSChecker/AutoHirsch
+ * 
+ * @author MX Programming Club 2016
+ * @version 10/15/2016
+ */
+
 package BSChecker;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
