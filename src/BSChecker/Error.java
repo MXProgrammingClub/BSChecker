7c6a226da79f8051e84849e79a835faf27d519c1
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-15 11:11:24 -0400

    Comments!

--- src/BSChecker/Error.java
+++ src/BSChecker/Error.java
@@ -1,6 +1,15 @@
 package BSChecker;
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author tedpyne
+ * Defines abstract class for types of grammatical errors
+ */
 public abstract class Error {
+	/**
+	 * 
+	 * @param text The block of text to find errors in
+	 * @return an ArrayList of int[2] pointers to the start and end of the error
+	 */
 	public abstract ArrayList<int[]> findErrors(String text);
 }
