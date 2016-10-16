d99af39e4312a1ecdbf40838794b97b672cf9d4f
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-16 10:07:41 -0400

    Finished model extraction

--- src/BSChecker/AmbiguousPronoun.java
+++ src/BSChecker/AmbiguousPronoun.java
@@ -29,7 +29,7 @@ public class AmbiguousPronoun extends Error {
 
 	public static void main (String[] args) {
 		String test = "I saw Mike and Bob. I also saw Ted and John, but I beat him.";
-		ArrayList<int[]> errors = new AmbiguousPronoun().findErrors(test);
+		ArrayList<int[]> errors = new AmbiguousPronoun().findErrors(test,null);
 //		for (int[] error : errors){
 //			System.out.println(error[0] + " " + error[1] + " " + error[2]);
 //		}
