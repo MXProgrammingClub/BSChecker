4476f0b79cf5c37213771702520c3826658df97f
mddks <megha.dalal@gmail.com>
2016-10-16 07:26:50 -0400

    BS error #2 (would SentenceDetector help?)

--- src/BSChecker/IncompleteSentences.java
+++ src/BSChecker/IncompleteSentences.java
@@ -3,7 +3,13 @@
  */
 package BSChecker;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 
 /**
  * @author Dalal
@@ -12,7 +18,7 @@ import java.util.ArrayList;
 public class IncompleteSentences extends Error {
 
 	public static void main (String[] args) {
-		ArrayList<int[]> errors = new IncompleteSentences().findErrors("Hi. How are you? This is Mike.");
+		ArrayList<int[]> errors = new IncompleteSentences().findErrors("Hi. How are. you? This is Mike.");
 		for (int[] error : errors)
 			System.out.println(error[0] + " " + error[1] + " " + error[2]);
 	}
@@ -20,8 +26,25 @@ public class IncompleteSentences extends Error {
 	@Override
 	public ArrayList<int[]> findErrors(String text) {
 		ArrayList<int[]> errors = new ArrayList<int[]>();
-
+		
+		
 		return errors;
 	}
+	
+	//SentenceDetector text first?
+	public static String[] SentenceDetect(String text) {
+		try {
+			// always start with a model, a model is learned from training data
+			InputStream is = new FileInputStream("en-sent.bin");
+			SentenceModel model = new SentenceModel(is);
+			SentenceDetectorME sdetector = new SentenceDetectorME(model);
+			is.close();
+			
+			return sdetector.sentDetect(paragraph);
+		}
+		catch (IOException e) {
+			return null;
+		}
+}
 
 }
