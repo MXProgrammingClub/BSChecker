dcaf7718fe356a75bb392d8848ec58b169a3e970
Ted Pyne <ted.pyne.4@gmail.com>
2016-10-15 20:42:38 -0400

    Tree class done

---
+++ src/BSChecker/SentenceTree.java
@@ -0,0 +1,26 @@
+package BSChecker;
+
+import java.util.ArrayList;
+
+import opennlp.tools.parser.Parse;
+
+public class SentenceTree {
+	private SentenceTree parent;
+	private ArrayList<SentenceTree> children;
+	private String label, tag;
+	public SentenceTree(SentenceTree parent, Parse parse){
+		label = parse.getLabel();
+		this.parent = parent;
+		tag = parse.getType();
+		children = new ArrayList<SentenceTree>();
+		Parse[] childs = parse.getChildren();
+		for(Parse child: childs){
+			children.add(new SentenceTree(this,child));
+		}
+	}
+	
+	public String getLabel(){return label;}
+	public String getTag(){return tag;}
+	public boolean hasChildren(){return children.size()==0;}
+	public ArrayList<SentenceTree> getChildren(){return children;}
+}
