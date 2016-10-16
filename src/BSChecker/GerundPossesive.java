64f43eddaa0ea518afa6c2cb9554b514cc5ee30a
jere99 <jrdegreeff@gmail.com>
2016-10-15 21:12:34 -0400

    GerundPossesive Class - please test for me

--- src/BSChecker/GerundPossesive.java
+++ src/BSChecker/GerundPossesive.java
@@ -19,18 +19,16 @@ import opennlp.tools.util.PlainTextByLineStream;
 
 /**
  * @author JeremiahDeGreeff
- * algorithms for progressive tense (error 12)
+ * algorithms for incorrect lack of possessives with gerunds (error 12)
  */
-public class ProgressiveTense extends Error {
+public class GerundPossesive extends Error {
 
-	private static final String[] TO_BE_CONJ = {"be", "am", "are", "is"};
-	
 	/**
 	 * for testing purposes
 	 */
 	public static void main(String[] args) {
-		Error testOb = new ProgressiveTense();
-		String testText = "Sensing God's desire to destroy Sodom, Abraham is negotiating for a less apocalyptic punishment\nJohn is fighting Harry while eating dougnuts";
+		Error testOb = new GerundPossesive();
+		String testText = "I saw the boy, running from danger";
 		testOb.findErrors(testText);
 	}
 
@@ -38,7 +36,7 @@ public class ProgressiveTense extends Error {
 	public ArrayList<int[]> findErrors(String text) {
 		POSModel model = new POSModelLoader().load(new File("lib/en-pos-maxent.bin"));
 		TokenizerModel tModel = null;
-		
+
 		InputStream is;
 		try {
 			is = new FileInputStream("lib/en-token.bin");
@@ -50,10 +48,10 @@ public class ProgressiveTense extends Error {
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		Tokenizer tokenizer = new TokenizerME(tModel);
 		POSTaggerME tagger = new POSTaggerME(model);
-		
+
 		ArrayList<ArrayList<int[]>> lineErrors = new ArrayList<ArrayList<int[]>>();
 		String line;
 
@@ -63,8 +61,8 @@ public class ProgressiveTense extends Error {
 			while ((line = lineStream.read()) != null) {
 				String[] tokenizerLine = tokenizer.tokenize(line);
 				String[] tags = tagger.tag(tokenizerLine);
-				
-				ArrayList<Integer> errorIndices = findProgressiveTense(tokenizerLine, tags);
+
+				ArrayList<Integer> errorIndices = findGerundPossesive(tokenizerLine, tags);
 				System.out.println();
 				lineErrors.add(findLoc(errorIndices, text, tokenizerLine));
 				System.out.println();
@@ -72,17 +70,17 @@ public class ProgressiveTense extends Error {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return combineLineErrors(lineErrors);
 	}
-	
+
 	/**
-	 * finds all the progressive tense errors in the given line
+	 * finds all the gerunds without possessives where there should be
 	 * @param tokenizerLine the tokens of the line
 	 * @param tags the array of the tag for each token
 	 * @return the indices of each token which is an error
 	 */
-	private ArrayList<Integer> findProgressiveTense(String[] tokenizerLine, String[] tags) {
+	private ArrayList<Integer> findGerundPossesive(String[] tokenizerLine, String[] tags) {
 		//finds gerunds and participles
 		ArrayList<Integer> errorIndices = new ArrayList<Integer>();
 		for(int i = 0; i < tags.length; i++)
@@ -94,21 +92,26 @@ public class ProgressiveTense extends Error {
 		for(int i = 0; i < errorIndices.size(); i++)
 			System.out.println(errorIndices.get(i) + ": " + tokenizerLine[errorIndices.get(i)]);
 
-		//checks if each gerund or participle has a form of "to be" before it
+		//checks if each gerund or participle has a non possessive noun or pronoun before it
 		int errorNum = 0;
-		String word = null;
+		String word, tag;
 		boolean isError;
 		while(errorNum < errorIndices.size()) {				
 			if(errorIndices.get(errorNum) == 0) {
 				errorIndices.remove(errorNum);
 			} else {
 				word = tokenizerLine[errorIndices.get(errorNum) - 1];
-				System.out.println((errorIndices.get(errorNum) - 1) + ": " + word);
+				tag = tags[errorIndices.get(errorNum) - 1];
+				System.out.println((errorIndices.get(errorNum) - 1) + ": " + word + " (" + tag + ")");
 				isError = false;
+				//Warning: will catch some cases which are not errors
 				for(int i = 0; i < 4; i++)
 				{
-					if(word.equals(TO_BE_CONJ[i]))
+					if(tag.equals("PRP"))
 						isError = true;
+					if(tag.equals("NN") || tag.equals("NNS") || tag.equals("NNP") || tag.equals("NNPS"))
+						if(word.indexOf('\'') == -1)
+							isError = true;
 				}
 				if(isError)
 					errorNum++;
@@ -116,10 +119,10 @@ public class ProgressiveTense extends Error {
 					errorIndices.remove(errorNum);
 			}
 		}
-		
+
 		return errorIndices;
 	}
-	
+
 	/**
 	 * finds indices in the original text of each error and updates result to include any new errors
 	 * @param errorIndices the indices of errors that have been found
@@ -130,7 +133,7 @@ public class ProgressiveTense extends Error {
 	private ArrayList<int[]> findLoc(ArrayList<Integer> errorIndices, String text, String[] tokenizerLine) {
 		int[] startIndeces = new int[errorIndices.size()], endIndeces = new int[errorIndices.size()];
 		int cursor = 0, start, end;
-		
+
 		for(int i = 0; i < errorIndices.size(); i++) {
 			System.out.println("error found: ");
 			System.out.println("\"" + tokenizerLine[errorIndices.get(i) - 1] + " " + tokenizerLine[errorIndices.get(i)] + "\"");
@@ -140,14 +143,14 @@ public class ProgressiveTense extends Error {
 			endIndeces[i] = end;
 			System.out.println("character indices: " + start + "-" + end);
 		}
-		
+
 		ArrayList<int[]> result = new ArrayList<int[]>();
 		result.add(startIndeces);
 		result.add(endIndeces);
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * combines the errors of each line into one ArrayList
 	 * @param lineErrors the errors from each line
@@ -157,9 +160,9 @@ public class ProgressiveTense extends Error {
 		int numErrors = 0;
 		for(int line = 0; line < lineErrors.size(); line++)
 			numErrors += lineErrors.get(line).get(0).length;
-		
+
 		int[] startIndeces = new int[numErrors], endIndeces = new int[numErrors];
-		
+
 		int errorNum = 0;
 		for(int line = 0; line < lineErrors.size(); line++)
 			for(int lineErrorNum = 0; lineErrorNum < lineErrors.get(line).get(0).length; lineErrorNum++) {
@@ -167,16 +170,15 @@ public class ProgressiveTense extends Error {
 				endIndeces[errorNum] = lineErrors.get(line).get(1)[lineErrorNum];
 				errorNum++;
 			}
-		
+
 		ArrayList<int[]> result = new ArrayList<int[]>();
 		result.add(startIndeces);
 		result.add(endIndeces);
-		
+
 		System.out.println("all found errors:");
 		for(int i = 0; i < result.get(0).length; i++) {
 			System.out.println(result.get(0)[i] + "-" + result.get(1)[i]);
 		}
 		return result;
 	}
-
 }
