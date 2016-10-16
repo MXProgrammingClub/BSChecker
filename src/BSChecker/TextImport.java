9905c3506ffe28fd17b2ef3b1dae7747b7685194
juliamcclellan <jmcclellan@mxschool.edu>
2016-10-15 22:21:38 -0400

    Saving text file

--- src/BSChecker/TextImport.java
+++ src/BSChecker/TextImport.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,7 +20,7 @@ import javafx.stage.FileChooser;
  * @author Julia
  */
 public class TextImport
-{	
+{
 	/**
 	 * Creates a file chooser for the user to select which file to open. Must be called from an event handler.
 	 * @return The selected file (null if no file is chosen).
@@ -50,7 +51,7 @@ public class TextImport
 	 * @param file The text file selected.
 	 * @return The text in the text file.
 	 */
-	public static String importTxt(File file)
+	private static String importTxt(File file)
 	{
 		Scanner scan = null;
 		try
@@ -77,7 +78,7 @@ public class TextImport
 	 * @param file The word document selected.
 	 * @return The text in the document.
 	 */
-	public static String importDoc(File file)
+	private static String importDoc(File file)
 	{
 		HWPFDocument doc = null;
 		try
@@ -103,7 +104,7 @@ public class TextImport
 	 * @param file The word document selected.
 	 * @return The text in the document.
 	 */
-	public static String importDocx(File file)
+	private static String importDocx(File file)
 	{
 		XWPFDocument doc = null;
 		try
@@ -126,5 +127,48 @@ public class TextImport
 			doc.close();
 		} catch (IOException e){} //should never happen
 		return text;
+	}
+	
+	/**
+	 * Saves the new text to the file.
+	 * @param file The file to save to
+	 * @param text The text to save.
+	 */
+	public static void saveText(File file, String text)
+	{
+		String extension = file.getName().substring(file.getName().indexOf('.'));
+		if(extension.equals(".txt"))
+		{
+			saveTxt(file, text);
+		}
+		else if(extension.equals(".doc"))
+		{
+			
+		}
+		else if(extension.equals(".docx"))
+		{
+			
+		}
+		else return; //should never happen
+	}
+	
+	/**
+	 * Saves the new text to the txt file.
+	 * @param file The file to save to
+	 * @param text The text to save.
+	 */
+	private static void saveTxt(File file, String text)
+	{
+		PrintWriter output = null;
+		try
+		{
+			output = new PrintWriter(file);
+		} catch (FileNotFoundException e){} //shouldn't happen
+		String[] paragraphs = text.split("\n");
+		for(String paragraph: paragraphs)
+		{
+			output.println(paragraph);
+		}
+		output.close();
 	}
 }
