44c10a196e657d96f7c3d3c32f28e35f44fc4391
mddks <megha.dalal@gmail.com>
2016-10-16 08:47:23 -0400

    Luke: Examples for bluesheets
    Thanks to Wani for typing out the examples.

--- src/BSChecker/Bluesheet.java
+++ src/BSChecker/Bluesheet.java
@@ -1,35 +1,30 @@
 /**
  * This enum represents the bluesheet errors.
  * @author Luke Giacalone
- * @version 10/15/2016
+ * @version 10/16/2016
  */
 
 package BSChecker;
 
 public enum Bluesheet {
 
-	PAST_TENSE ("Past Tense", "Use the present tense in writing about a literary work.", 1),
-	COMPLETE_SENTENCE ("Fragment/Run-On/Comma-Splice", "Write complete, corrext sentences.", 2),
-	FIRST_SECOND_PERSON ("First/Second Person", "Do not use the first or second person (\"I,\" \"me,\" \"my\"; \"we,\" \"us,\" \"our\"; \"you,\" \"your) in critical writing", 3),
-	VAGUE_THIS_WHICH ("Vague \"this\" or \"which\"", "Do not use \"this\" or \"which\" to refer to a clause.", 4),
-	SUBJECT_VERB_AGREEMENT ("Subject-Verb Agreement", "A subject and verb must agree in number (sungular, plural). A pronoun must agree in number (singular, plural) with its antecedent.", 5),
-	PRONOUN_CASE ("Pronoun Case", "Put pronouns in the appropriate case (subjective, objective, possessive)", 6),
-	AMBIGUOUS_PRONOUN ("Ambiguous Pronoun", "Avoid ambiguous pronouns.", 7),
-	APOSTROPHE_ERROR ("Apostrophe Error", "Use an apostrophe to indicate possession, not to indicate that a noun is plural. Distinguish properly between its and it's.", 8),
-	PASSIVE_VOICE ("Passive Voice", "Write in the active voice; avoid the passive voice.", 9),
-	DANGLING_PARTICIPLE ("Dangling Participle", "Avoid dangling modifiers.", 10),
-	FAULTY_PARALLELISM ("Faulty Parallelism", "Use identical grammatical forms to coordinate parallel ideas.", 11),
-	PROGRESSIVE_TENSE ("Progressive Tense", "Avoid progressive tenses.", 12),
-	GERUNDS ("Incorrect Use of Gerund/Possessive", "Recognize gerunds and use possessives accordingly - WARNING this may catch some false \"errors\"", 13),
-	QUOTATION ("Quotation Error", "Malformed Quotation and/or Citation", 14);
+	FIRST_SECOND_PERSON ("First/Second Person", "Do not use the first or second person (\"I,\" \"me,\" \"my\"; \"we,\" \"us,\" \"our\"; \"you,\" \"your) in critical writing", "(Incorrect) I think that Holden Caulfield, the hero of The Catcher in the Rye, is actually a hypocrite.\n(Correct) Holden Caulfield, the hero if the Catcher in the Rye, is actually a hypocrite. ", 3),
+	PRONOUN_CASE ("Pronoun Case", "Put pronouns in the appropriate case (subjective, objective, possessive)", "(Incorrect) She is the last person who I would suspect. \n(Correct) She is the last person whom I would suspect.\n\n(Incorrect) Give the credit to she and me\n(Correct) Give the credit to her and me.", 6),
+	AMBIGUOUS_PRONOUN ("Ambiguous Pronoun", "Avoid ambiguous pronouns.", "(Incorrect) Oedipus and the shepherd argue about whether he should know the truth. \n(Correct) Oedipus and the shepherd argue about whether Oedipus should know the truth.", 7),
+	PASSIVE_VOICE ("Passive Voice", "Write in the active voice; avoid the passive voice.", "(Incorrect) Gulliver is taught many lessons in rational behavior. \n(Correct) The Houyhnhnms teach Gulliver many lessons in rational behavior. ", 9),
+	FAULTY_PARALLELISM ("Faulty Parallelism", "Use identical grammatical forms to coordinate parallel ideas.", "(Incorrect) In a good essay the sentences are clear, concise, and hang together. \n(Correct) In a good essay, the sentences are clear, concise, and coherent.", 11),
+	GERUNDS ("Incorrect Use of Gerund/Possessive", "Recognize gerunds and use possessives accordingly", "(Incorrect) Elizabeth is grateful for him loving her so well. \n(Correct) Elizabeth is grateful for his loving her so well. ", 13),
+	QUOTATION ("Quotation Error", "Malformed Quotation and/or Citation", "Punctuation goes inside the quotations.\nCitations go outside the quotations.\nUse commas to introduce a quote preceeded by a verb of saying or thinking.", 14);
 
 	private final String name;
 	private final String description;
+	private final String example;
 	private final int number;
 
-	Bluesheet(String name, String description, int number) {
+	Bluesheet(String name, String description, String example, int number) {
 		this.name = name;
 		this.description = description;
+		this.example = example;
 		this.number = number;
 	}
 
@@ -39,6 +34,10 @@ public enum Bluesheet {
 
 	public String getDescription() {
 		return description;
+	}
+	
+	public String getExample() {
+		return example;
 	}
 
 	public int getNumber() {
