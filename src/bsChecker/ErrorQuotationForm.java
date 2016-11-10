8a2dd8b6799d58ba7e869de2f23c55547f2b95ae
jere99 <jrdegreeff@gmail.com>
2016-11-09 23:37:56 -0500

    now works with token return system
    still need to add something to catch when the start and end quotes are
    on the same token

--- src/bsChecker/ErrorQuotationForm.java
+++ src/bsChecker/ErrorQuotationForm.java
@@ -14,8 +14,9 @@ public class ErrorQuotationForm extends Error {
 	private static final int ERROR_NUMBER = 14;	
 	private static final String FILE_NAME = "SayingVerbs.txt"; //the location of the list of verbs of saying or thinking
 	private static final HashSet<String> VERB_SET = importVerbs(); //the set of verbs of saying or thinking
-	private static final String[] PUNCTUATION = {".", ",", ":", ";", "?", "!"};
-
+	private static final String[] PUNCTUATION1 = {".", ","};
+	private static final String[] PUNCTUATION2 = {":", ";"};
+	
 	/**
 	 * Imports the list of words of saying or thinking.
 	 * @return The set of words.
@@ -41,7 +42,7 @@ public class ErrorQuotationForm extends Error {
 	 */
 	public static void main (String[] args) {
 		Error.setupOpenNLP();
-		String input = "";
+		String input = "he says \"hi there.\" he says, \"hi there.\" he says: \"hi there.\"";
 		System.out.println("\ninput: " + input + "\n");
 		ArrayList<int[]> errors = new ErrorQuotationForm().findErrors(input);
 		sort(errors);
@@ -50,6 +51,7 @@ public class ErrorQuotationForm extends Error {
 
 	/**
 	 * finds all errors with quotation form in the given paragraph
+	 * known issues: doesn't see a preceding verb if other words between it and the quote, doesn't work with single word quotes
 	 * @param line paragraph to check
 	 * @return ArrayList int[3] representing errors where [0] is the beginning token index, [1] is ending token index, [2] is the type of error (9)
 	 */
@@ -57,34 +59,24 @@ public class ErrorQuotationForm extends Error {
 	public ArrayList<int[]> findErrors(String line)
 	{
 		String tokens[] = tokenizer.tokenize(line);
-		
 		ArrayList<int[]> errors = new ArrayList<int[]>();
-		for(int i = 0, count = 0; i < tokens.length; i++)
-		{
+		for(int i = 0; i < tokens.length; i++)
 			if(tokens[i].contains("\"")) //finds opening quotation
-			{
 				for(int j = i + 1; j < tokens.length; j++)
-				{
-					if(tokens[j].contains("\"")) //finds ending quotation
-					{
-						if(findErrorsFront(tokens, i, j))
-						{
-							int loc = Error.locationOf(line, "\"", count);
-							if(line.charAt(loc - 3) == ',') errors.add(new int[]{loc - 3, loc - 3, ERROR_NUMBER});
-							else errors.add(new int[]{line.lastIndexOf(' ', loc - 3) + 1, loc - 1, ERROR_NUMBER});
-						}
-						if(findErrorsBack(tokens, i, j))
-						{
-							int loc = Error.locationOf(line, "\"", count + 1);
-							errors.add(new int[]{loc, loc, ERROR_NUMBER});
-						}
+					if(tokens[j].contains("\"")) { //finds ending quotation
+						int errorFront = findErrorsFront(tokens, i, j);
+						int errorBack = findErrorsBack(tokens, i, j);
+						if(errorFront == 1 || errorFront == 2 || errorFront == 3)
+							errors.add(new int[] {i - 1, i - 1, ERROR_NUMBER});
+						if(errorBack == 1 || errorBack == 3)
+							errors.add(new int[] {j - 1, j - 1, ERROR_NUMBER});
+						if(errorBack == 2)
+							errors.add(new int[] {j + 1, j + 1, ERROR_NUMBER});
+						
 						i = j;
-						count += 2;
 						break;
 					}
-				}
-			}
-		}
+		
 		return errors;
 	}
 
@@ -93,21 +85,20 @@ public class ErrorQuotationForm extends Error {
 	 * @param tokens The tokens from the nlp tokenizer.
 	 * @param start The starting index of the quotation.
 	 * @param end The ending index of the quotation
-	 * @return true if an error was found, false otherwise.
+	 * @return 0 if no error, 1 if semicolon that should be a comma, 2 if comma that should be no punctuation, 3 if no punctuation that should be a comma
 	 */
-	private boolean findErrorsFront(String[] tokens, int start, int end)
+	private int findErrorsFront(String[] tokens, int start, int end)
 	{
-		if(tokens[start - 1].equals(":")) return false; //there is no issue associated with running in a quotation with a colon
-		else if(tokens[start - 1].equals(",")) //if there is a comma before, the word before should be a verb
-		{
-			if(VERB_SET.contains(tokens[start - 2])) return false;
-			else return true;
-		}
-		else //if there is a word before, it should not be a verb
-		{
-			if(VERB_SET.contains(tokens[start - 1])) return true;
-			else return false;
-		}
+		if(tokens[start - 1].equals(":")) {
+			if(VERB_SET.contains(tokens[start - 2]))
+				return 1; //error if there is a semicolon before and the word before it is a verb
+		} else if(tokens[start - 1].equals(",")) {
+			if(!VERB_SET.contains(tokens[start - 2]))
+				return 2; //error if there is a comma before and the word before it is not a verb
+		} else {
+			if(VERB_SET.contains(tokens[start - 1]))
+				return 3; //error if there is a not punctuation before and the word before is a verb
+		} return 0;
 	}
 
 	/**
@@ -115,30 +106,17 @@ public class ErrorQuotationForm extends Error {
 	 * @param tokens The tokens from the nlp tokenizer.
 	 * @param start The starting index of the quotation.
 	 * @param end The ending index of the quotation
-	 * @return true if an error is found, false otherwise
+	 * @return 0 if no error, 1 if cited with punctuation incorrectly inside, 2 if not cited with punctuation incorrectly outside, 3 if not cited and punctuation incorrectly inside
 	 */
-	private boolean findErrorsBack(String[] tokens, int start, int end)
-	{
-		if(tokens[end].contains("(") || end + 1 < tokens.length && tokens[end + 1].contains("("))
-		{
-			if(isPunctuation(tokens[end - 1])) return true; //error if cited and punctuation inside
-			else return false;
-		}
-		else if(end + 1 < tokens.length && isPunctuation(tokens[end + 1])) return true; //error if not cited and punctuation outside
-		else return false;
-	}
-
-	/**
-	 * Returns whether the given string is in the array.
-	 * @param str The possible punctuation.
-	 * @return Whether str is punctuation.
-	 */
-	private boolean isPunctuation(String str)
-	{
-		for(String punc: PUNCTUATION)
-		{
-			if(str.equals(punc)) return true;
-		}
-		return false;
+	private int findErrorsBack(String[] tokens, int start, int end) {
+		if(tokens[end].contains("(") || end + 1 < tokens.length && tokens[end + 1].contains("(")) {
+			if(arrayContains(PUNCTUATION1, tokens[end - 1]) || arrayContains(PUNCTUATION2, tokens[end - 1]))
+				return 1; //error if cited and punctuation inside
+		} else {
+			if(end + 1 < tokens.length && arrayContains(PUNCTUATION1, tokens[end + 1]))
+				return 2; //error if not cited and period/comma outside
+			if(arrayContains(PUNCTUATION2, tokens[end - 1]))
+				return 3; //error if not cited and colon/semicolon inside
+		} return 0;
 	}
 }
