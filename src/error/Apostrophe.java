8d9da4842b8c8c7c0bf27d48c812792159b0e6e1
jere99 <jrdegreeff@gmail.com>
2017-01-23 13:48:29 -0500

    everything looks good; removed WIP comments

--- src/error/Apostrophe.java
+++ src/error/Apostrophe.java
@@ -4,10 +4,9 @@ import util.ErrorList;
 import util.UtilityMethods;
 
 /**
- * WIP
  * Finds apostrophe errors. (8)
- * @author JeremiahDeGreeff
  * @author tedpyne
+ * @author JeremiahDeGreeff
  */
 public class Apostrophe extends Error {
 	/**
@@ -39,7 +38,7 @@ public class Apostrophe extends Error {
 	}
 
 	/**
-	 * WIP
+	 * Finds omissions of apostrophes and incorrect apostrophes in the passed line of text
 	 * @param line the paragraph in which to find errors
 	 * @return an ErrorList of int[3] pointers to the indices of the start and end tokens of an error
 	 * 			int[0], int[1] are start and end tokens of the error
@@ -59,6 +58,7 @@ public class Apostrophe extends Error {
 				//If the preceding word is a noun, the tag of noun is highly likely to be in error. e.g. "the poem features cars", the tag "features"->"NNS" is incorrect
 				if((i==0 || !((tags[i-1].length()>1 && tags[i-1].substring(0, 2).equals("NN")) || tags[i-1].equals("WDT"))) && tags[j].length()>1 && tags[j].substring(0,2).equals("NN"))
 					errors.add(new int[]{i, j, ERROR_NUMBER});
+				
 				if(i+1 < tokens.length && tags[i+1].length()>2 && tags[i+1].substring(0, 3).equals("POS")){
 					j = i+2;
 					while((tags[j].substring(0,2).equals("RB") || tags[j].substring(0,2).equals("JJ")) && j < tokens.length)
