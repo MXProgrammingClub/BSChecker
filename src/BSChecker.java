50f1b886e21fb325760ed6d57463984768534dfe
jere99 <jrdegreeff@gmail.com>
2017-01-31 21:11:51 -0500

    Started playing around with parser

--- src/BSChecker.java
+++ src/BSChecker.java
@@ -3,9 +3,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.cmdline.postag.*;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.parser.Parse;
+import opennlp.tools.parser.Parser;
+import opennlp.tools.parser.ParserFactory;
+import opennlp.tools.parser.ParserModel;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
 import opennlp.tools.postag.POSTaggerME;
@@ -21,11 +26,12 @@ import opennlp.tools.util.*;
  */
 public class BSChecker {
 	public static void main(String[] args) throws InvalidFormatException, IOException{
-		String input = "";
-		Tokenize(input);
-		SentenceDetect(input);
-		POSTag(input);
-		findName(input);
+		String input = "This text is an example sentence.";
+//		Tokenize(input);
+//		SentenceDetect(input);
+//		POSTag(input);
+//		findName(input);
+		parse(input);
 	}
 
 	public static void Tokenize(String input) throws InvalidFormatException, IOException {
@@ -34,22 +40,22 @@ public class BSChecker {
 		is.close();
 		Tokenizer tokenizer = new TokenizerME(model);
 		
-		String tokens[] = tokenizer.tokenize(input);
+		String[] tokens = tokenizer.tokenize(input);
 		
-		for (String a : tokens)
-			System.out.println(a);
+		for (String token : tokens)
+			System.out.println(token);
 	}
 
-	public static void SentenceDetect(String paragraph) throws InvalidFormatException, IOException {
+	public static void SentenceDetect(String input) throws InvalidFormatException, IOException {
 		InputStream is = new FileInputStream("lib/en-sent.bin");
 		SentenceModel model = new SentenceModel(is);
 		is.close();
 		SentenceDetectorME sdetector = new SentenceDetectorME(model);
 
-		String sentences[] = sdetector.sentDetect(paragraph);
+		String[] sentences = sdetector.sentDetect(input);
 
-		System.out.println(sentences[0]);
-		System.out.println(sentences[1]);
+		for(String sentence : sentences)
+			System.out.println(sentence);
 	}
 
 	public static void POSTag(String input) throws IOException {
@@ -79,9 +85,20 @@ public class BSChecker {
 		NameFinderME nameFinder = new NameFinderME(model);
 
 		String[] tokens = tokenizer.tokenize(input);
-		Span nameSpans[] = nameFinder.find(tokens);
+		Span[] nameSpans = nameFinder.find(tokens);
 
 		for(Span s: nameSpans)
 			System.out.println(s.toString());			
+	}
+	
+	public static void parse(String input) throws IOException {
+		InputStream is = new FileInputStream("lib/en-parser-chunking.bin");
+		ParserModel pModel = new ParserModel(is);
+		is.close();
+		Parser parser = ParserFactory.create(pModel);
+		
+		Parse[] topParses = ParserTool.parseLine(input, parser, 1);
+		for(Parse p : topParses)
+			p.show();
 	}
 }
