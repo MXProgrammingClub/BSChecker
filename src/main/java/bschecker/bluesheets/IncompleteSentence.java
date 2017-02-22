cf4f209c69081b45d2f1293781ac2d6cb8a8923f
jere99 <jrdegreeff@gmail.com>
2017-02-21 19:24:48 -0500

    boilerplate code for sentence structure

--- src/main/java/bschecker/bluesheets/IncompleteSentence.java
+++ src/main/java/bschecker/bluesheets/IncompleteSentence.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import main.java.bschecker.util.TokenErrorList;
 import main.java.bschecker.util.Tools;
+import main.java.bschecker.util.UtilityMethods;
 
 /**
  * WIP
@@ -41,13 +42,34 @@ public class IncompleteSentence extends Bluesheet {
 	}
 
 	/**
-	 * WIP
 	 * @param line the paragraph in which to find errors
 	 * @return a TokenErrorList of int[3] elements where [0] and [1] are start and end tokens of the error and [2] is the error number (2)
 	 */
 	@Override
 	protected TokenErrorList findErrors(String line) {
 		TokenErrorList errors = new TokenErrorList(line);
+		String[] sentences = Tools.getSentenceDetector().sentDetect(line);
+		int tokenOffset = 0;
+		for(String sentence : sentences){
+			errors.addAll(findErrorsInSentence(sentence, tokenOffset));
+			tokenOffset += Tools.getTokenizer().tokenize(sentence).length;
+		}
+		return errors;
+	}
+	
+	/**
+	 * WIP
+	 * finds any issues in the structure of a sentence
+	 * @param sentence the sentence to examine
+	 * @param tokenOffset the number of tokens which have occurred in earlier sentences (for returning purposes)
+	 * @return a TokenErrorList of int[3] elements where [0] and [1] are start and end tokens of the error and [2] is the error number (2) which represents all the errors in this sentence
+	 */
+	private TokenErrorList findErrorsInSentence(String sentence, int tokenOffset) {
+		TokenErrorList errors = new TokenErrorList(sentence);
+		String parsedText = UtilityMethods.parse(sentence);
+		System.out.println("\n" + parsedText);
+		
+		
 		return errors;
 	}
 }
