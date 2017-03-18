9370f11617aac431126258534a5ba1d04329aaaa
jere99 <jrdegreeff@gmail.com>
2017-03-18 14:57:05 -0400

    keeping track of times for each bluesheet to process

--- src/main/java/bschecker/bluesheets/Bluesheet.java
+++ src/main/java/bschecker/bluesheets/Bluesheet.java
@@ -43,10 +43,12 @@ public abstract class Bluesheet {
 	 * @return a ErrorList which contains all the errors in the passage, referenced by character indices
 	 */
 	public static ErrorList findAllErrors(String text) {
+		long start = System.currentTimeMillis();
 		ErrorList errors = new ErrorList(text, false);
 		int lineNum = 1, charOffset = 0;
 		String line;
 		while (charOffset < text.length()) {
+			long lineStart = System.currentTimeMillis();
 			line = text.substring(charOffset, charOffset + text.substring(charOffset).indexOf('\n'));
 			
 			System.out.println("\nAnalyzing line " + lineNum + " (characters " + charOffset + "-" + (charOffset + line.length()) + "):");
@@ -56,20 +58,21 @@ public abstract class Bluesheet {
 			
 			ErrorList lineErrors = new ErrorList(line, true);
 			for(Bluesheets b : Bluesheets.values())
-				if(b.getBluesheetObj().CheckedWhenAnalyzed) {
+				if(b.getBluesheetObj().CheckedWhenAnalyzed){
+					long bluesheetStart = System.currentTimeMillis();
 					System.out.print("\tlooking for: " + b.getName() + "... ");
 					ErrorList temp = b.getBluesheetObj().findErrors(line);
-					System.out.println(temp.size() + (temp.size() == 1 ? " Error" : " Errors") + " Found");
 					lineErrors.addAll(temp);
+					System.out.println(temp.size() + (temp.size() == 1 ? " Error" : " Errors") + " Found (" + ((System.currentTimeMillis() - bluesheetStart) / 1000d) + "s)");
 				}
-			System.out.println(lineErrors.size() + (lineErrors.size() == 1 ? " Error" : " Errors") + " Found in line " + lineNum);
+			System.out.println(lineErrors.size() + (lineErrors.size() == 1 ? " Error" : " Errors") + " Found in line " + lineNum + " (" + ((System.currentTimeMillis() - lineStart) / 1000d) + "s)");
 			lineErrors.sort();
 			errors.addAll(lineErrors.tokensToChars(charOffset, removedChars));
 
 			lineNum++;
 			charOffset += line.length() + removedChars.size() + 1;
 		}
-		System.out.println("\n\n" + errors);
+		System.out.println("\n\nPassage analyzed in " + ((System.currentTimeMillis() - start) / 1000d) + "s\n\n" + errors);
 		
 		return errors;
 	}
