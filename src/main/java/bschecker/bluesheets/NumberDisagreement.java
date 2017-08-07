b6f7db55f47e1c231be4bf2542ffdcc4baf36595
jere99 <jrdegreeff@gmail.com>
2017-08-07 00:31:56 -0400

    remove old code
    400th commit!

--- src/main/java/bschecker/bluesheets/NumberDisagreement.java
+++ src/main/java/bschecker/bluesheets/NumberDisagreement.java
@@ -19,22 +19,8 @@ public class NumberDisagreement extends Bluesheet {
 	 */
 	@Override
 	protected ErrorList findErrors(String line, Parse[] parses){
-//		String sentences[] = Tools.getSentenceDetector().sentDetect(line);
-//		ArrayList<Parse> parses = new ArrayList<Parse>();
-//		ArrayList<int[]> arr = new ArrayList<int[]>();
-//		for(String s: sentences){
-//			ParserTool.parseLine(s.substring(0, s.length()-1), Tools.getParser(), 1)[0].show();
-//			correctParse(ParserTool.parseLine(s.substring(0,s.length()-1), Tools.getParser(), 1)[0]);
-//			arr.addAll(correctParse(ParserTool.parseLine(s.substring(0,s.length()-1), UtilityMethods.getParser(), 1)[0]));
-//		}
 		ErrorList errors = new ErrorList(line);
 		return errors;
 	}
-//	public static ArrayList<int[]> correctParse(Parse p){
-//		System.out.println(p);
-//		SentenceTree head = new SentenceTree(null,p.getChildren()[0]);
-//		SentenceTree tree = head.fix();
-//		return new ArrayList<int[]>();
-//	}
 	
 }
